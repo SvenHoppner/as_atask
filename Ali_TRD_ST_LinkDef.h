2c87331df84f425e7b2f578c65e0a61a6c7abeea
A. Bórquez <andres.borquezc@gmail.com>
2022-06-24 11:09:08 +0200

    First commit: - Copied necessary files from Alex's directory - Added .gitignore

---
+++ Ali_TRD_ST_LinkDef.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+
+#pragma link C++ nestedclass;
+#pragma link C++ class Ali_MC_particle+;
+#pragma link C++ class Ali_TRD_ST_TOF_hit+;
+#pragma link C++ class Ali_TRD_ST_Tracklets+;
+#pragma link C++ class Ali_TRD_ST_TPC_Track+;
+#pragma link C++ class Ali_TRD_ST_Event+;
+#pragma link C++ class Ali_Helix+;
+
+
+#endif
